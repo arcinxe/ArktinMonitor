b74340ed485813b785eadcd103e2e32587946f69
Arcin <m.arkan@student.po.edu.pl>
2017-10-17 19:56:05 +0200

    Initial commit

---
+++ ArktinMonitor.WebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ArktinMonitor.WebApp.MvcApplication" Language="C#" %>
---
+++ ArktinMonitor.WebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ArktinMonitor.WebApp.Startup))]
+namespace ArktinMonitor.WebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
