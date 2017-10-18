47b6b639409a970da3d96af0f98cfb74f3a5da6f
Arcin <m.arkan@student.po.edu.pl>
2017-10-18 17:36:52 +0200

    Add ComputerController and fixes serialization of data retrieved from database

--- ArktinMonitor.WebApp/Global.asax.cs
+++ ArktinMonitor.WebApp/Global.asax.cs
@@ -13,6 +13,12 @@ namespace ArktinMonitor.WebApp
     {
         protected void Application_Start()
         {
+            // https://stackoverflow.com/questions/23098191/failed-to-serialize-the-response-in-web-api-with-json
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            //GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Serialize;
+            GlobalConfiguration.Configuration.Formatters.Remove(
+                GlobalConfiguration.Configuration.Formatters.XmlFormatter);
+
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
